59efb274e5174c972d62b1d306a8b26857d9cd04
Pieter Wuille <pieter.wuille@gmail.com>
2017-06-30 17:35:42 -0700

    Introduce secp256k1_ecmult_strauss

--- src/ecmult.h
+++ src/ecmult.h
@@ -25,7 +25,17 @@ static void secp256k1_ecmult_context_clone(secp256k1_ecmult_context *dst,
 static void secp256k1_ecmult_context_clear(secp256k1_ecmult_context *ctx);
 static int secp256k1_ecmult_context_is_built(const secp256k1_ecmult_context *ctx);
 
-/** Double multiply: R = na*A + ng*G */
+#ifdef USE_ENDOMORPHISM
+#define STRAUSS_MAX_POINTS 24
+#else
+#define STRAUSS_MAX_POINTS 32
+#endif
+
+/** Multi-multiply: R = na[0]*A[0] + na[1]*A[1] + ... + na[n-1]*A[n-1] + ng*G. ng can be NULL */
+static void secp256k1_ecmult_strauss(const secp256k1_ecmult_context *ctx, secp256k1_gej *r, int n, const secp256k1_gej *a, const secp256k1_scalar *na, const secp256k1_scalar *ng);
+
+/** Double multiply: R = na*A + ng*G. ng can be NULL */
 static void secp256k1_ecmult(const secp256k1_ecmult_context *ctx, secp256k1_gej *r, const secp256k1_gej *a, const secp256k1_scalar *na, const secp256k1_scalar *ng);
+
 
 #endif
